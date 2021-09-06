f4b5eaa0de4d7144cc582afadb6423e5dad3faaa
Bryan Daniel <brdaniel117@gmail.com>
2021-09-06 10:53:00 -0700

    almost finished work on thought.js

--- routes/api/thought.js
+++ routes/api/thought.js
@@ -1,6 +1,11 @@
+const { getThoughts, createThought, getThoughById, updateThought, deleteThought } = require('../../controllers/thoughtController')
+const { route } = require('./user')
+
 const router = require('express').Router()
 
-
+router.route('/').get(getThoughts).post(createThought)
+router.route('/:id').get(getThoughById).put(updateThought).delete(deleteThought)
+router.route('/:thoughtId/reaction').post().delete()
 
 
 
